35c2d095292d57bd308a4ca3e75083360c5a61aa
GopalsamyKannan <gopalsamykannan@gmail.com>
2025-01-12 14:01:01 +0530

    API Parameterization

--- src/test/java/stepDefinitions/APITestSteps.java
+++ src/test/java/stepDefinitions/APITestSteps.java
@@ -6,6 +6,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import utils.ConfigReader;
 
 import java.util.Set;
 
@@ -13,15 +14,22 @@ public class APITestSteps {
 
     Response response;
 
-    @Given("I send a GET request to {string}")
-    public void sendGetRequest(String endpoint) {
-        RestAssured.baseURI = "https://api.coindesk.com";
+    //@Given("I send a GET request to {string}")
+    @Given("I send a GET request to the endpoint")
+    public void sendGetRequest() {
+     /*   RestAssured.baseURI = "https://api.coindesk.com";
+        response = RestAssured.get(endpoint);*/
+
+        RestAssured.baseURI = ConfigReader.getProperty("baseURI");
+        String endpoint = ConfigReader.getProperty("endpoint");
         response = RestAssured.get(endpoint);
     }
 
-    @Then("I should get a {int} status code")
-    public void verifyStatusCode(int statusCode) {
-        Assert.assertEquals("Status code mismatch!", statusCode, response.getStatusCode());
+    //@Then("I should get a {int} status code")
+    @Then("the status code should be {int}")
+    public void verifyStatusCode(int expectedStatusCode) {
+        //Assert.assertEquals("Status code mismatch!", expectedStatusCode, response.getStatusCode());
+        Assert.assertEquals("Unexpected status code!", expectedStatusCode, response.getStatusCode());
     }
 
     @Then("the response should contain BPIs {string}, {string}, and {string}")
--- src/test/resources/features/APITest.feature
+++ src/test/resources/features/APITest.feature
@@ -2,7 +2,7 @@ Feature: Verify Coindesk API
 
   @APISmoke
   Scenario: Validate BPI data and descriptions
-    Given I send a GET request to "/v1/bpi/currentprice.json"
-    Then I should get a 200 status code
+    Given I send a GET request to the endpoint
+    Then the status code should be 200
     And the response should contain BPIs "USD", "GBP", and "EUR"
     And "GBP" description should be "British Pound Sterling"
